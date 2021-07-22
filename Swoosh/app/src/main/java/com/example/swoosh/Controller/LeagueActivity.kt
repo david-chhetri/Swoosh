672938ccd0a6fac44ef4cb877bf685a4fcc70206
davidchhetri <david_chhetri@yahoo.com>
2021-07-22 16:24:01 -0400

    lesson 10

--- Swoosh/app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
+++ Swoosh/app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
@@ -13,6 +13,20 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("","")
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER,player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        if (savedInstanceState != null) {
+            super.onRestoreInstanceState(savedInstanceState)
+            player = savedInstanceState.getParcelable<Player>(EXTRA_PLAYER)!!
+        }
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
--- Swoosh/app/src/main/java/com/example/swoosh/Controller/SkillActivity.kt
+++ Swoosh/app/src/main/java/com/example/swoosh/Controller/SkillActivity.kt
@@ -29,6 +29,18 @@ class SkillActivity : BaseActivity() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER,player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if( savedInstanceState != null) {
+            player = savedInstanceState.getParcelable<Player>(EXTRA_PLAYER)!!
+        }
+    }
+
     fun onBeginnerSkillClick(view: View){
 
         ballerSkillBtn.isChecked = false
